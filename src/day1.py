64b0ff05837a23d7b65a7d2a71edfe8061575256
Raul Jimenez <rauljmz@gmail.com>
2021-12-02 15:47:47 +0000

    Refactor to use sample files with new lines

--- src/day1.py
+++ src/day1.py
@@ -3,15 +3,16 @@ from functools import reduce
 from typing import Iterable
 
 def count_increases(values: Iterable):
-    return reduce(lambda a, y: (y, a[1] + 1 if a[0] != None and y > a[0] else a[1]), values, (None, 0))[1]
+    return reduce(lambda a, y: (y, a[1] + 1 if a[0] is not None and y > a[0] else a[1]), values, (None, 0))[1]
 
 def window3(values: Iterable):
     return list(map(lambda x,y,z: x+y+z, values, values[1:], values[2:]))
 
 if __name__ == "__main__":
     path = sys.argv[1]
-    file = open(path, "r")
-    values = file.read().split(",")
-    values = list(map(lambda x: int(x), values))
-    print(f"{len(values)} values")
-    print(f"{count_increases(window3(values))} are increasing over the previous")
+    data = []
+    with open(path, "r", encoding="utf-8") as file:
+        for line in file:
+            data.append(int(line))
+    print(f"{len(data)} values")
+    print(f"{count_increases(window3(data))} are increasing over the previous")
