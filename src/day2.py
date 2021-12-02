f6b496466cd4f3186aefe2756119becbb21f41bc
Raul Jimenez <rauljmz@gmail.com>
2021-12-02 22:07:45 +0000

    Day 2 part 2

--- src/day2.py
+++ src/day2.py
@@ -1,15 +1,18 @@
 import sys
 import numpy as np
-from functools import reduce 
+from functools import reduce
 
 moves = {
-    "forward": (1,0),
-    "up": (0,-1),
-    "down": (0,1),
+    "forward": (1,1,0),
+    "up": (0,0,-1),
+    "down": (0,0,1),
 }
 
 def move(position, step):
-    return position + (np.array(moves.get(step[0])) * step[1])
+    move = np.array(moves.get(step[0]))
+    count = step[1]
+    current_aim = position[2]
+    return position + move * np.array([count,count*current_aim,count])
 
 def main(path):
     data = []
@@ -17,7 +20,9 @@ def main(path):
         for line in file:
             (direction, count) = line.split(" ")
             data.append((direction, int(count)))
-    return np.prod(reduce(move, data, np.array([0,0])))
+
+    final_position = reduce(move, data, np.array([0,0,0]))[:2]
+    return np.prod(final_position)
 
 if __name__ == "__main__":
     print(f"{main(sys.argv[1])} result of multiplying position")
--- src/day2_test.py
+++ src/day2_test.py
@@ -2,4 +2,4 @@ import day2
 
 class TestDay2:
     def test_sample_data_returns_150(self):
-        assert day2.main('./data/day2/test.txt') == 150
+        assert day2.main('./data/day2/test.txt') == 900
