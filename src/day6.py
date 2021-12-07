706e0123cae37d3a6c82912bb337a32e77604908
Raul Jimenez <rauljmz@gmail.com>
2021-12-07 11:22:40 +0000

    day6 part 2

--- src/day6.py
+++ src/day6.py
@@ -1,7 +1,15 @@
 import sys
 import numpy as np
 
+def memoize(f):
+    memo = {}
+    def helper(x,y):
+        if (x,y) not in memo:
+            memo[(x,y)] = f(x,y)
+        return memo[(x,y)]
+    return helper
 
+@memoize
 def count_from_single_fish(initial, days):
     count = 1
     for reproduction_day in range(initial + 1, days + 1, 7):
@@ -14,12 +22,11 @@ def count_fish(path, days):
         initial_states = np.fromstring(file.readline(), sep=',', dtype=np.uint)
 
     fn = np.frompyfunc(count_from_single_fish, 2, 1)
-    return fn(initial_states, days).sum()
 
-
-def part1(path):
-    return count_fish(path, 80)
+    return fn(1,np.arange(days,days-6,-1))[initial_states-1].sum()
 
 
 if __name__ == "__main__":
-    print(f"{part1(sys.argv[1])} fish")
+    days = int(sys.argv[1])
+    path = sys.argv[2]
+    print(f"{count_fish(path, days)} fish after {days} days")
