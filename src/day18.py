3b09bd728697fa2af997ef88eefc8e98a734776e
Raul <rauljmz@gmail.com>
2021-12-20 17:51:46 +0000

    Day 18 - explodes

--- src/day18.py
+++ src/day18.py
@@ -8,7 +8,7 @@ class Symbol:
 
     def set_next(self, next):
         self.next = next
-        
+
     def __str__(self):
         return self.value
 
@@ -22,23 +22,30 @@ class Symbol:
         if value in mapping:
             return mapping[value](previousvalue)
         else:
-            return Symbol(value, previousvalue)
+            return Digit(value, previousvalue)
+
 
 class BracketOpen(Symbol):
     def __init__(self, previous):
         super().__init__("[", previous)
 
+
 class BracketClose(Symbol):
     def __init__(self, previous):
         super().__init__("]", previous)
+
 
 class Separator(Symbol):
     def __init__(self, previous):
         super().__init__(",", previous)
 
+
 class Digit(Symbol):
     def __init__(self, value, previous):
-        super().__init__(value, previous)
+        super().__init__(int(value), previous)
+    
+    def __str__(self):
+        return str(self.value)
 
 
 class Number:
@@ -47,11 +54,36 @@ class Number:
         for symbol in representation:
             symbols.append(
                 Symbol.build(symbol, symbols[-1] if len(symbols) > 0 else None)
-                )
+            )
         self.first_symbol = symbols[0]
 
     def explode(self):
-        pass
+        depth = 0
+        symbol = self.first_symbol
+        while depth < 5 or not isinstance(symbol, Digit):
+            if isinstance(symbol, BracketOpen):
+                lastOpen = symbol
+                depth += 1
+            elif isinstance(symbol, BracketClose):
+                depth -= 1
+            symbol = symbol.next
+        if symbol:
+            value = symbol.value
+            prev_symbol = symbol.previous
+            while not isinstance(prev_symbol, Digit) and prev_symbol:
+                prev_symbol = prev_symbol.previous
+            if prev_symbol:
+                prev_symbol.value += value
+            
+            value = symbol.next.next.value
+            next_symbol = symbol.next.next.next
+            while not isinstance(next_symbol, Digit) and next_symbol:
+                next_symbol = next_symbol.next
+            if next_symbol:
+                next_symbol.value += value
+            close = symbol.next.next.next
+            zero = Digit(0,lastOpen.previous)
+            zero.set_next(close.next)
 
     def __iter__(self):
         self.current = self.first_symbol
@@ -66,4 +98,4 @@ class Number:
             raise StopIteration
 
     def __str__(self):
-        return "".join(map(str,self))
+        return "".join(map(str, self))
--- src/day18_test.py
+++ src/day18_test.py
@@ -1,3 +1,4 @@
+import pytest
 from day18 import Number
 
 class TestNumber:
@@ -7,7 +8,14 @@ class TestNumber:
         number = Number(self.st_input)
         assert str(number) == self.st_input
 
-    def xtest_explode_number(self):
-        number = Number(self.st_input)
+    @pytest.mark.parametrize("st_number, st_exploded", [
+        ( st_input, "[[[[0,9],2],3],4]"),
+        ( "[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]"),
+        ( "[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]"),
+        ( "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"),
+        ( "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"),
+    ])
+    def test_exploding_numbers(self, st_number, st_exploded):
+        number = Number(st_number)
         number.explode()
-        assert str(number) == "[[[[0,9],2],3],4]"
+        assert str(number) == st_exploded
